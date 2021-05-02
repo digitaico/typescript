46a55fb2fbc82e76c151a0705378a72c3782563b
Jorge Eduardo Ardila <jeam@tuta.io>
2021-05-02 17:47:58 -0500

    first commit

--- main.ts
+++ main.ts
@@ -1,8 +1,3 @@
-let f = (a: number) => {
-	a = 6;
-	return a;
-}
-
 enum Role {SUPERVISOR, READ_ONLY, AUTHOR = 7, GUEST }
 
 const person : {
@@ -11,12 +6,14 @@ const person : {
 	hobbies: string[];
 	role: Role;
 	status : [number, string];
+	is_member: boolean;
 }= {
 	name: 'Alexander',
 	age: 30,
 	hobbies: ['Hunting', 'War'],
 	role: Role.AUTHOR,
-	status: [3, 'ACTIVE']
+	status: [3, 'ACTIVE'],
+	is_member: true,
 }
 
 console.log(person.role);
